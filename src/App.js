e4fc8f9176cd45b30352c6148f43db11f02e6eae
chyerin802 <chyerin802@gmail.com>
2022-12-16 16:40:28 +0900

    feat: Simple Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import { Provider } from 'react-redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+import Counter from './Counter';
 import logo from './logo.svg';
 import './App.css';
 
+export const counterSlice = createSlice({
+  name: 'counterSlice',
+  initialState: { value: 0 },
+  reducers: {
+    // action의 형태를 지정
+    increment: (state, action) => {
+      //state.value += action.step;
+      console.log(action); // {type: 'counterSlice/increment', payload: 2}
+      state.value += action.payload;
+    },
+    decrement: (state, action) => {
+      //state.value -= action.step;
+      state.value -= action.payload;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    // counter에 대한 reducer는 counterSlice
+    // counter라는 작은 store를 만든 것
+    counter: counterSlice.reducer,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Counter></Counter>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
